6ce15e513412faa54b0d0a6068480449136e4a13
fojlyR <frabbybics10@gmail.com>
2023-08-08 23:54:07 +0600

    new update

--- src/back.js
+++ src/back.js
@@ -19,7 +19,7 @@ app.get("/", (req, res) => {
   res.json({ status: "success" });
 });
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.resolve(__dirname, "./build")));
   
   app.get("*", function (request, response) {
